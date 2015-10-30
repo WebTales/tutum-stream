a187ebad88d138d3858233e31cc0b6ae90585cae
Didier Fanchon <didier.fanchon@webtales.fr>
2015-10-30 14:08:25 +0100

    Get uuid from ressource_uri

--- client.py
+++ client.py
@@ -1,19 +1,10 @@
 import os
 import tutum
- 
-def on_error(event):
-    print error
- 
-def on_close():
-    print "### closed ###"
- 
+
 def on_message(event):
     if event.get("state", "") not in ["In progress", "Pending", "Terminating", "Starting", "Scaling", "Stopping"] and event.get("type", "").lower() in ["container", "service"]:
-        msg = "Tutum event: %s %s is %s" % (event["type"], parse_uuid_from_resource_uri(event.get("resource_uri", "")), event["state"].lower())
+        msg = "Tutum event: %s %s is %s" % (event["type"], str(event.get("resource_uri", "")).split('/')[-2], event["state"].lower())
         print(msg)
-
-def on_open():
-    print "Connected"
  
 if __name__ == "__main__":
     events = tutum.TutumEvents()
